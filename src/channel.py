3cc0352752d1624e03845734f0ec0733cce8e4eb
kirillskyeng <kirill.netreba@skyeng.ru>
2023-05-15 11:08:00 +0300

    Change sample channels

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,23 @@
+import json
+import os
+
+from googleapiclient.discovery import build
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """Класс для ютуб - канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        """
+        Экземпляр инициализируется id канала.
+        Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        api_key: str = os.getenv('API_KEY_YT')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels()
+        channel = channel.list(id=self.channel_id, part='snippet,statistics')
+        channel = channel.execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
